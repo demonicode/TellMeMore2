d403240abb7e6bf77ad259f6e8c2976d89807ec3
demonicode <shikharbansal111@gmail.com>
2016-12-29 16:00:15 +0530

    2nd commit

--- app/src/main/java/com/example/shikhar/tellmemore2/MainActivity.java
+++ app/src/main/java/com/example/shikhar/tellmemore2/MainActivity.java
@@ -1,23 +1,27 @@
 package com.example.shikhar.tellmemore2;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.ScrollingView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
-
+public class
 public class MainActivity extends AppCompatActivity {
 
     private CoordinatorLayout mContainer;
     private static String[] mPermissions = { Manifest.permission.ACCESS_FINE_LOCATION};
     private static final int PERMISSIONS_REQUEST_CODE = 100;
     private static final String TAG = "MainActivity";
+    private ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +36,15 @@ public class MainActivity extends AppCompatActivity {
             startActivity(bluetoothIntent);
         }
 
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+
+        mContainer = (LinearLayout) findViewById(R.id.activity_main);
+
+        ImageView photo = (ImageView)findViewById(R.id.falana);
+        ScrollingView info = (ScrollingView)findViewById(R.id.falana);
+
+        
 
     }
     private boolean isBlueEnable() {
