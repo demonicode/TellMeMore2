bef86490f55392cbb7d0a6d6ac986950e636f14f
demonicode <shikharbansal111@gmail.com>
2016-12-29 14:11:01 +0530

    2nd commit

--- app/src/main/java/com/example/shikhar/tellmemore2/MainActivity.java
+++ app/src/main/java/com/example/shikhar/tellmemore2/MainActivity.java
@@ -1,13 +1,58 @@
 package com.example.shikhar.tellmemore2;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CoordinatorLayout mContainer;
+    private static String[] mPermissions = { Manifest.permission.ACCESS_FINE_LOCATION};
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (!havePermissions()) {
+            Log.i(TAG, "Requesting permissions needed for this app.");
+            requestPermissions();
+        }
+
+        if(!isBlueEnable()){
+            Intent bluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(bluetoothIntent);
+        }
+
+
+    }
+    private boolean isBlueEnable() {
+        BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+        BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+        return bluetoothAdapter.isEnabled();
+
+    }
+
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this,
+                mPermissions, PERMISSIONS_REQUEST_CODE);
+    }
+
+    private boolean havePermissions() {
+        for(String permission:mPermissions){
+            if(ActivityCompat.checkSelfPermission(this,permission)!= PackageManager.PERMISSION_GRANTED){
+                return  false;
+            }
+        }
+        return true;
     }
 }
